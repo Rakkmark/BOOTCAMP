278b822f277c9b52fc16cf0011607c3e3c382beb
RakkMark <markkoenig03@gmail.com>
2022-04-05 11:39:20 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
     darkMode: 'class',
     content: [
         './src/**/*.{html,js}',
-        'index.html'
+        'index.html',
+        './src/index.html'
     ],
 
     theme: {
