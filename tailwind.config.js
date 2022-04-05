8eb47565d1722edb277e55f640ee3b07dbd44238
RakkMark <markkoenig03@gmail.com>
2022-04-05 08:23:31 -0400

    Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    darkMode: 'class',
+    content: [
+        './src/**/*.{html,js}',
+        'stylingHTMLpages.html'
+    ],
+
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
